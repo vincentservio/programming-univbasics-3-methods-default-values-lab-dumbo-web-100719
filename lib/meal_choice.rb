023a150484948229496237eff31d7e602c52a45b
vincent servio <vincentservio@gmail.com>
2019-08-30 18:40:59 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -5,7 +5,7 @@
 def meal_choice (broccoli,macaroni,protein = "meat")
 
 p "A plate of #{protein} with #{broccoli} and #{macaroni}."
-p ("What a nutritious meal!"   #{meal_choice})
+puts ("What a nutritious meal!"   #{meal_choice})
 end
 
 
